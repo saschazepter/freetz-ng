2d27844680ad4f9536d73686f4c86d3ba264b0eb
Ircama <Ircama@users.noreply.github.com>
2025-10-21 08:52:53 +0200

    add file 5.45 utility package (#1280)
    This PR adds support for the file utility, which determines file types by examining their content.
    
    Features:
    - Full file type detection functionality
    - External files configuration
    - Documentation included
    
    Extracted from commit 80650c67434b30f7398f7b2378f5258399da7f1f
    
    Co-authored-by: Ircama <Ircama>

---
+++ docs/make/file.md
@@ -0,0 +1,8 @@
+# file 5.45 (binary only)
+  - Homepage: [https://www.darwinsys.com/file/](https://www.darwinsys.com/file/)
+  - Manpage: [https://linux.die.net/man/1/file](https://linux.die.net/man/1/file)
+  - Changelog: [https://github.com/file/file/releases](https://github.com/file/file/releases)
+  - Repository: [https://github.com/file/file](https://github.com/file/file)
+  - Package: [master/make/pkgs/file/](https://github.com/Freetz-NG/freetz-ng/tree/master/make/pkgs/file/)
+  - Maintainer: [@Ircama](https://github.com/Ircama)
+
---
+++ make/pkgs/file/file.mk
@@ -0,0 +1,48 @@
+$(call PKG_INIT_BIN, 5.45)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_HASH:=fc97f51029bb0e2c9f4e3bffefdaf678f0e039ee872b9de5c002a6d09c784d82
+$(PKG)_SITE:=http://ftp.astron.com/pub/file,ftp://ftp.astron.com/pub/file
+### WEBSITE:=https://www.darwinsys.com/file/
+### MANPAGE:=https://linux.die.net/man/1/file
+### CHANGES:=https://github.com/file/file/releases
+### CVSREPO:=https://github.com/file/file
+### SUPPORT:=Ircama
+
+$(PKG)_BINARY_BUILD := $($(PKG)_DIR)/src/file
+$(PKG)_BINARY_TARGET := $($(PKG)_DEST_DIR)/usr/bin/file
+
+$(PKG)_MAGIC_BUILD := $($(PKG)_DIR)/magic/magic.mgc
+$(PKG)_MAGIC_TARGET := $($(PKG)_DEST_DIR)/usr/share/misc/magic.mgc
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --disable-shared
+$(PKG)_CONFIGURE_OPTIONS += --disable-libseccomp
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY_BUILD): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(FILE_DIR)
+
+$($(PKG)_BINARY_TARGET): $($(PKG)_BINARY_BUILD)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_MAGIC_TARGET): $($(PKG)_MAGIC_BUILD)
+	$(INSTALL_FILE)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARY_TARGET) $($(PKG)_MAGIC_TARGET)
+
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(FILE_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $($(PKG)_BINARY_TARGET)
+	$(RM) $($(PKG)_MAGIC_TARGET)
+	$(RM) -r $($(PKG)_DEST_DIR)/usr/share/misc
+
+$(PKG_FINISH)
