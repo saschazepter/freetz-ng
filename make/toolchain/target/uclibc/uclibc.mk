8082b7ba84802e24f9f73c8e49cab651e4207313
fda77 <fda77@users.noreply.github.com>
2025-10-22 14:56:16 +0200

    uclibc: add oldconfig support

--- make/toolchain/target/uclibc/uclibc.mk
+++ make/toolchain/target/uclibc/uclibc.mk
@@ -166,14 +166,14 @@ uclibc-menuconfig: $(UCLIBC_DIR)/.config
 	cp -f $^ $(UCLIBC_CONFIG_FILE) && \
 	touch $^
 
-uclibc-olddefconfig: $(UCLIBC_DIR)/.config
+uclibc-oldconfig uclibc-olddefconfig: $(UCLIBC_DIR)/.config
 	$(UCLIBC_MAKE) -C $(UCLIBC_DIR) \
 		$(UCLIBC_COMMON_BUILD_FLAGS) \
 		PREFIX=$(TARGET_TOOLCHAIN_DIR)/$(UCLIBC_DEVEL_SUBDIR)/ \
 		DEVEL_PREFIX=/usr/ \
 		RUNTIME_PREFIX=$(TARGET_TOOLCHAIN_DIR)/$(UCLIBC_DEVEL_SUBDIR)/ \
 		HOSTCC="$(TOOLCHAIN_HOSTCC) $(UCLIBC_HOST_CFLAGS)" \
-		olddefconfig && \
+		$(if $(FREETZ_TARGET_UCLIBC_0),oldconfig,olddefconfig)
 	cp -f $^ $(UCLIBC_CONFIG_FILE) && \
 	touch $^
 
@@ -317,7 +317,7 @@ uclibc_target-dirclean:
 uclibc_target-distclean: uclibc_target-dirclean
 
 
-.PHONY: uclibc-source uclibc-unpacked uclibc-autofix uclibc-menuconfig uclibc-olddefconfig uclibc-configured
+.PHONY: uclibc-source uclibc-unpacked uclibc-autofix uclibc-menuconfig uclibc-oldconfig uclibc-olddefconfig uclibc-configured
 .PHONY: uclibc        uclibc-clean        uclibc-dirclean        uclibc-distclean
 .PHONY: uclibc_target uclibc_target-clean uclibc_target-dirclean uclibc_target-distclean
 
