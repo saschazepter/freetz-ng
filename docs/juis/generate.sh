4fec0139a1e0b33ae2a3ab60f82f8ccb7b71d393
fda77 <fda77@users.noreply.github.com>
2025-10-31 15:13:54 +0100

    juis: update seq

--- docs/juis/generate.sh
+++ docs/juis/generate.sh
@@ -40,21 +40,24 @@ cat fos-xxx | while read -s x; do sed "/^${x//\//\\\/}$/d" -i fos-lab fos-inh; d
 #cat fos-xxx | while read -s x; do sed "/\/${x##*/}$/d" -i fos-lab fos-inh; done
 
 
+#dect
+seq="$((for x in $(seq 1 13); do seq ${x}01 ${x}05; done; seq 606 615) | sort -n)"
+
 #dect-rel
 echo -e '\n### Dect-Release ###############################################'
-for x in $(seq  10 139); do [ ${#x} != 3 ] && x="0$x"; x="${x::-1}.0${x:2}";
+for x in $seq; do [ ${#x} != 4 ] && x="0$x"; x="${x::-2}.${x:2}";
                              env - $TOOLS/juis_check --dect HW=154 DHW=$x                             -a; done | tee dect-rel
 #dect-lab
 echo -e '\n### Dect-Labor #################################################'
-for x in $(seq  10 139); do [ ${#x} != 3 ] && x="0$x"; x="${x::-1}.0${x:2}";             m="226.08.00-115000"
+for x in $seq; do [ ${#x} != 4 ] && x="0$x"; x="${x::-2}.${x:2}";                        m="226.08.00-115000"
                              env - $TOOLS/juis_check --dect HW=154 DHW=$x Buildtype=1000  Version=$m  -a; done | tee dect-lab
-for x in $(seq  10 139); do [ ${#x} != 3 ] && x="0$x"; x="${x::-1}.0${x:2}";             m="226.07.50-100000"
+for x in $seq; do [ ${#x} != 4 ] && x="0$x"; x="${x::-2}.${x:2}";                        m="226.07.50-100000"
                              env - $TOOLS/juis_check --dect HW=154 DHW=$x Buildtype=1000  Version=$m  -a; done | tee dect-lab -a
 #dect-inh
 echo -e '\n### Dect-Inhaus ################################################'
-for x in $(seq  10 139); do [ ${#x} != 3 ] && x="0$x"; x="${x::-1}.0${x:2}";             m="226.08.00-115000"
+for x in $seq; do [ ${#x} != 4 ] && x="0$x"; x="${x::-2}.${x:2}";                        m="226.08.00-115000"
                              env - $TOOLS/juis_check --dect HW=154 DHW=$x Buildtype=1001  Version=$m  -a; done | tee dect-inh
-for x in $(seq  10 139); do [ ${#x} != 3 ] && x="0$x"; x="${x::-1}.0${x:2}";             m="226.07.50-100000"
+for x in $seq; do [ ${#x} != 4 ] && x="0$x"; x="${x::-2}.${x:2}";                        m="226.07.50-100000"
                              env - $TOOLS/juis_check --dect HW=154 DHW=$x Buildtype=1001  Version=$m  -a; done | tee dect-inh -a
 #dect-sub
 cat dect-rel | while read -s x; do sed "/\/${x##*/}$/d" -i dect-lab dect-inh; done
