3aab3120c71fe742dc1532fc68ed34e2fe35e64e
fda77 <fda77@users.noreply.github.com>
2025-10-31 02:23:09 +0100

    fix binutils-tools: remove code clones

--- make/pkgs/binutils-tools/binutils-tools.mk
+++ make/pkgs/binutils-tools/binutils-tools.mk
@@ -1,33 +1,23 @@
 $(call PKG_INIT_BIN, 2.45)
 $(PKG)_SOURCE:=binutils-$($(PKG)_VERSION).tar.xz
 $(PKG)_HASH:=c50c0e7f9cb188980e2cc97e4537626b1672441815587f1eab69d2a1bfbef5d2
-$(PKG)_SITE:=https://ftp.gnu.org/gnu/binutils,https://mirror.dogado.de/gnu/binutils
+$(PKG)_SITE:=@GNU/binutils
 ### WEBSITE:=https://www.gnu.org/software/binutils/
 ### MANPAGE:=https://sourceware.org/binutils/docs/
-### CHANGES:=https://sourceware.org/binutils/docs-2.45/binutils/
+### CHANGES:=https://sourceware.org/git/gitweb.cgi?p=binutils-gdb.git;a=blob_plain;f=binutils/NEWS
 ### CVSREPO:=https://sourceware.org/git/binutils-gdb.git
 ### SUPPORT:=Ircama
 
 $(PKG)_CATEGORY:=Debug helpers
 
-# List of all available binutils tools
-$(PKG)_BINUTILS_SIMPLE := readelf objdump objcopy strings ar ranlib addr2line size
-
-# Selected binutils tools based on user configuration  
-$(PKG)_BINUTILS_SIMPLE_SELECTED := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_BINUTILS_SIMPLE))
-$(PKG)_BINUTILS := $($(PKG)_BINUTILS_SIMPLE_SELECTED)
-ifneq ($(strip $(FREETZ_PACKAGE_BINUTILS_TOOLS_NM)),)
-$(PKG)_BINUTILS += nm
-endif
-ifneq ($(strip $(FREETZ_PACKAGE_BINUTILS_TOOLS_STRIP)),)
-$(PKG)_BINUTILS += strip
-endif
-
-# Build directory paths for binutils tools
-$(PKG)_BINUTILS_SIMPLE_BUILD_DIR := $($(PKG)_BINUTILS_SIMPLE:%=$($(PKG)_DIR)/binutils/%)
-
-# Target directory paths for binutils tools
-$(PKG)_BINUTILS_TARGET_DIR := $($(PKG)_BINUTILS:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+$(PKG)_SRC_POSTFIX:=-new
+$(PKG)_SRC_BIN:=ar addr2line nm-new objcopy objdump ranlib readelf size strings strip-new
+$(PKG)_DST_BIN:=$(patsubst %$($(PKG)_SRC_POSTFIX),%,$($(PKG)_SRC_BIN))
+$(PKG)_SEL_BIN:=$(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_DST_BIN))
+$(PKG)_SRC_DIR:=$($(PKG)_SRC_BIN:%=$($(PKG)_DIR)/binutils/%)
+$(PKG)_DST_DIR:=$($(PKG)_DST_BIN:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+$(PKG)_SEL_DIR:=$($(PKG)_SEL_BIN:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+$(PKG)_EXCLUDED:=$(filter-out $($(PKG)_SEL_DIR),$($(PKG)_DST_DIR))
 
 $(PKG)_CONFIGURE_OPTIONS += --target=$(REAL_GNU_TARGET_NAME)
 $(PKG)_CONFIGURE_OPTIONS += --disable-shared
@@ -40,38 +30,24 @@ $(PKG)_CONFIGURE_OPTIONS += --without-included-gettext
 $(PKG)_CONFIGURE_OPTIONS += --enable-deterministic-archives
 
 
-# Standard download and unpack for binutils
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-# Build all binutils tools together
-$($(PKG)_BINUTILS_SIMPLE_BUILD_DIR) $(BINUTILS_TOOLS_DIR)/binutils/nm-new $(BINUTILS_TOOLS_DIR)/binutils/strip-new: $(BINUTILS_TOOLS_DIR)/.configured
+$($(PKG)_SRC_DIR): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(BINUTILS_TOOLS_DIR)
 
-# Install binutils simple tools (no rename needed)
-$(foreach binary,$($(PKG)_BINUTILS_SIMPLE_BUILD_DIR),$(eval $(call INSTALL_BINARY_STRIP_RULE,$(binary),/usr/bin)))
-
-# Install binutils tools with renamed outputs
-ifeq ($(strip $(FREETZ_PACKAGE_BINUTILS_TOOLS_NM)),y)
-$(BINUTILS_TOOLS_DEST_DIR)/usr/bin/nm: $(BINUTILS_TOOLS_DIR)/binutils/nm-new
-	$(INSTALL_BINARY_STRIP)
-endif
-
-ifeq ($(strip $(FREETZ_PACKAGE_BINUTILS_TOOLS_STRIP)),y)
-$(BINUTILS_TOOLS_DEST_DIR)/usr/bin/strip: $(BINUTILS_TOOLS_DIR)/binutils/strip-new
-	$(INSTALL_BINARY_STRIP)
-endif
+$(foreach binary,$($(PKG)_SRC_DIR),$(eval $(call INSTALL_BINARY_STRIP_RULE,$(binary),/usr/bin,,$(patsubst %$($(PKG)_SRC_POSTFIX),%,$(binary)))))
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_BINUTILS_TARGET_DIR)
+$(pkg)-precompiled: $($(PKG)_DST_DIR)
 
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(BINUTILS_TOOLS_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) $($(PKG)_BINUTILS_TARGET_DIR)
+	$(RM) $(BINUTILS_TOOLS_DST_DIR)
 
 $(PKG_FINISH)
