7b6311e81048404de9a296cf0b6a1844d1b25004
Ircama <Ircama>
2025-11-23 07:56:55 +0100

    Fix prelink-host download

--- make/host-tools/prelink-host/prelink-host.mk
+++ make/host-tools/prelink-host/prelink-host.mk
@@ -1,7 +1,7 @@
 $(call TOOLS_INIT, 20131005)
 $(PKG)_SOURCE:=prelink_0.0.$($(PKG)_VERSION).orig.tar.bz2
 $(PKG)_HASH:=1a5bf03381e83fbfbbe98ecca7b58ce2e726f662b560c3ff05aebcdaae397130
-$(PKG)_SITE:=https://people.redhat.com/jakub/prelink,@DEBIAN/prelink
+$(PKG)_SITE:=https://snapshot.debian.org/archive/debian/20200101T000000Z/pool/main/p/prelink,http://mirror.fiu.edu/ubuntu/pool/universe/p/prelink
 ### WEBSITE:=https://people.redhat.com/jakub/prelink/
 ### MANPAGE:=https://people.redhat.com/jakub/prelink/prelink.pdf
 ### CHANGES:=https://packages.debian.org/buster/execstack
